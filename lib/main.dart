a01211b5091d882801b00c67753c467db58b2aca
Michael Barilla <michael@eeproto.com>
2022-07-30 12:18:04 -0400

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+// 1) Create a new Flutter App (in this project) and output an AppBar and some text
+// below it
+// 2) Add a button which changes the text (to any other text of your choice)
+// 3) Split the app into three widgets: App, TextControl & Text
+
+import 'package:flutter/material.dart';
+
+void main() => runApp(App());
+
+class App extends StatefulWidget {
+  @override
+  State<App> createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Flutter Assignment'),
+        ),
+        body: Text('Some text in the body!'),
+      ),
+    );
+  }
+}
